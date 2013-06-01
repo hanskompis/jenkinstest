d3d70732ff5377c26818aea4c755c3c9da9d0615
Hansi Keijonen <renejacquesfury@gmail.com>
2013-06-01 12:32:31 +0300

    initial commit

---
+++ src/test/java/com/mycompany/jankinstest/AppTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.jankinstest;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring-context.xml"})
+public class AppTest {
+
+    @BeforeClass
+    public static void setUpClass() {}
+
+    @AfterClass
+    public static void tearDownClass() {}
+
+    @Before
+    public void setUp() {}
+
+    @After
+    public void tearDown() {}
+
+    @Test
+    public void youShallPass() {
+
+        Assert.assertTrue(true);
+    }
+}
